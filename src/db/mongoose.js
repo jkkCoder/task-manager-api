80b37dd3fe3bfb4a60e9b143cdbaefe799fc1a79
jkkCoder <jayeshk118@gmail.com>
2021-09-15 19:13:24 +0530

    added few more validation and sanitization

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -28,36 +28,52 @@ const User = mongoose.model("User",{
                 throw new Error("Age must be a positive number")
             }
         }
+    },
+    password:{
+        type:String,
+        required:true,
+        trim:true,
+        validate(value){
+            if(value.length<7){
+                throw new Error("Password length too short")
+            }
+            if(value.toLowerCase().includes("password")){
+                throw new Error("password cannot contain `password`")
+            }
+        }
     }
 })
 
-const me = new User({
-    name:"        jkkCoder",
-    email:"JAYESHK118@gmail.com    "
-})
+// const me = new User({
+//     name:"        jkkCoder",
+//     email:"JAYESHK118@gmail.com    ",
+//     password:"jayeshkk220022"
+// })
 
-me.save().then(()=>{
-    console.log(me)
-}).catch((error)=>{
-    console.log("Error!",error)
-})
+// me.save().then(()=>{
+//     console.log(me)
+// }).catch((error)=>{
+//     console.log("Error!",error)
+// })
 
 const Task = mongoose.model("Task",{
     description:{
-        type:String
+        type:String,
+        required:true,
+        trim:true
     },
     completed:{
-        type:Boolean
+        type:Boolean,
+        default:false
     }
 })
 
-// const task = new Task({
-//     description:"buy television",
-//     completed:false
-// })
+const task = new Task({
+    description:"buy refrigerator"
+})
 
-// task.save().then(()=>{
-//     console.log(task)
-// }).catch((err)=>{
-//     console.log(err)
-// })
+task.save().then(()=>{
+    console.log(task)
+}).catch((err)=>{
+    console.log(err)
+})
