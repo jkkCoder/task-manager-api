b0b88a362a1c7ac85fda59770c1704aba8d1a8f1
jkkCoder <jayeshk118@gmail.com>
2021-09-16 08:45:12 +0530

    added async await in playground

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -3,11 +3,23 @@ const Task = require("../src/models/task")
 
 // 6140971acd8fdd4e8013d82f
 
-Task.findByIdAndDelete("6140971acd8fdd4e8013d82f").then((user)=>{
-    console.log(user)
-    return Task.find({comleted:false})
-}).then((tasks)=>{
-    console.log(tasks)
+// Task.findByIdAndDelete("6140971acd8fdd4e8013d82f").then((user)=>{
+//     console.log(user)
+//     return Task.find({comleted:false})
+// }).then((tasks)=>{
+//     console.log(tasks)
+// }).catch((e)=>{
+//     console.log(e)
+// })
+
+const deleteTaskAndCount = async (id)=>{
+    const user = await Task.findByIdAndDelete(id)
+    const count = await Task.countDocuments({complete:false})
+    return count 
+}
+
+deleteTaskAndCount("6141f826222c7eb3edfbe9bb").then((count)=>{
+    console.log(count)
 }).catch((e)=>{
     console.log(e)
 })
--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -3,11 +3,23 @@ const User = require("../src/models/user")
 
 // 614208d52a9aa5717fc436ce
 
-User.findByIdAndUpdate("614208d52a9aa5717fc436ce",{age:1}).then((user)=>{
-    console.log(user)
-    return User.countDocuments({age:1})      //promise chaining
-}).then((result)=>{
-    console.log(result)
+// User.findByIdAndUpdate("614208d52a9aa5717fc436ce",{age:1}).then((user)=>{
+//     console.log(user)
+//     return User.countDocuments({age:1})      //promise chaining
+// }).then((result)=>{
+//     console.log(result)
+// }).catch((e)=>{
+//     console.log(e)
+// })
+
+const updateAgeAndCount = async(id,age)=>{
+    const user = await User.findByIdAndUpdate(id,{age:age})
+    const count = await User.countDocuments({age:age})
+    return count
+}
+
+updateAgeAndCount("614208d52a9aa5717fc436ce",2).then((count)=>{
+    console.log(count)
 }).catch((e)=>{
     console.log(e)
 })
