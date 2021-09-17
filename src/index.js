5d6d3ace22cccc54dfc1fa53abca5ed37821090a
jkkCoder <jayeshk118@gmail.com>
2021-09-17 21:03:37 +0530

    playing with bcryptjs

--- src/index.js
+++ src/index.js
@@ -23,3 +23,19 @@ app.use(taskRouter)
 app.listen(port,()=>{
     console.log("server is up on port",port)
 })
+
+const bcrypt = require("bcryptjs")
+
+const myFunction = async ()=>{
+    const password = "Red12345"
+    const hashedPassword = await bcrypt.hash(password,8)
+
+    console.log(password)
+    console.log(hashedPassword)
+
+    //to check while loggin in plain text password matches with hashed pass
+    const isMatch = await bcrypt.compare("Red12345",hashedPassword)
+    console.log(isMatch)
+}
+
+myFunction()
